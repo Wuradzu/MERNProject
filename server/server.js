21d0ad83f90e4dc0821072127372ff50a6bbd1d7
Wuradzu <savinykhvladislav@gmail.com>
2023-02-06 16:56:24 +0200

    ADD: middleware

--- server/server.js
+++ server/server.js
@@ -1,26 +1,35 @@
 const express = require("express");
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const { errorHandler } = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOprions");
 
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+app.use(logger);
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 
-app.all('*', (req, res) =>{
-    res.status(404)
-    if(req.accepts('html')){
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } else if(req.accepts('json')){
-        res.json({message: '404 Resource Not Found'})
-    } else{
-        res.type('txt').send('404 Resource Not Found')
-    }
-})
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Resource Not Found" });
+  } else {
+    res.type("txt").send("404 Resource Not Found");
+  }
+});
 
-// app.get("/", (req, res) => {
-//   res.status(200).json("server worked");
-// });
+app.use(errorHandler);
 
 const asyncStart = () => {
   app.listen(PORT, () => {
